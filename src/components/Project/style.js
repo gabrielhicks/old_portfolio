63a3c41d2765c1ff67d4133a1071db3041e214f1
gabrielhicks <gabrielhicks@mail.com>
2020-11-16 22:53:58 -0500

    added pointer

--- src/components/Project/style.js
+++ src/components/Project/style.js
@@ -17,4 +17,10 @@ export const ProjectCard = styled.div`
     &:hover {
         box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     }
+    @media (max-width: 800px) {
+        margin-bottom: 3vh;
+        height: 50vh;
+        font-size: 0.9rem;
+    }
+
 `
