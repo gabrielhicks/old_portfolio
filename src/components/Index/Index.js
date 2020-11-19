aa8b95faa2cdecb3b4b7b95079620ab3b0f4b89e
gabrielhicks <gabrielhicks@mail.com>
2020-11-19 13:32:52 -0500

    fixing heading

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -6,11 +6,7 @@ export default function Index() {
     return (
         <IndexWrapper>
             <h1>Gabriel Hicks</h1>
-            <li>This website is currently under construction, however you can find me at the following locations</li>
-            <li>LinkedIn: <a alt="LinkedIn Profile" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/gabrieljhicks/">Profile</a></li>
-            <li>Github: <a alt="Github Repo" target="_blank" rel="noreferrer" href="https://www.github.com/gabrielhicks/">Repositories</a></li>
-            <li>Technical Blog: <a alt="Medium Blog" target="_blank" rel="noreferrer" href="https://gabrielhicks.medium.com/">Medium</a></li>
-            <li>Or checkout my live projects below!</li>
+            <h3>LinkedIn: <a alt="LinkedIn Profile" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/gabrieljhicks/">Profile</a>&nbsp;|&nbsp;Github: <a alt="Github Repo" target="_blank" rel="noreferrer" href="https://www.github.com/gabrielhicks/">Repositories</a>&nbsp;|&nbsp;Technical Blog: <a alt="Medium Blog" target="_blank" rel="noreferrer" href="https://gabrielhicks.medium.com/">Medium</a></h3>
             <Projects />
         </IndexWrapper>
     )
