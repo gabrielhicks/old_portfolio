ba6169a19e040177b6196ed74e4c15c41ad93faf
gabrielhicks <gabrielhicks@mail.com>
2020-12-05 18:42:03 -0500

    Fixing styling

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
+import {ButtonStyled, BottomCardActions} from './style'
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import WebIcon from '@material-ui/icons/Web';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const useStyles = makeStyles({
     root: {
@@ -18,21 +20,6 @@ const useStyles = makeStyles({
         height: 240,
     },
 });
-        // <ProjectCard>
-        //     <h2>{props.project.name}</h2>
-        //     <ProjectImage>
-        //         <img src={props.project.image} />
-        //     </ProjectImage>
-        //     <ProjectDetails>
-        //         <p>{props.project.description}</p>
-        //         {props.project.video_url.length !== 0 ? <><a target="_blank" rel="noreferrer" alt="Hosted website" href={props.project.video_url}>YouTube</a></> : null}
-        //         {props.project.url.length !== 0 ? <><a target="_blank" rel="noreferrer" alt="Hosted website" href={props.project.url}>Live Demo</a></> : null }
-        //         {props.project.front.length !== 0 ? <><a target="_blank" rel="noreferrer" alt="Frontend Repo" href={props.project.front}>Frontend</a></> : null }
-        //         {props.project.back.length !== 0 ? <><a target="_blank" rel="noreferrer" alt="Backend Repo" href={props.project.back}>Backend</a></> : null }
-        //         {/* <>Technologies used: {props.project.tech}</> */}
-        //     </ProjectDetails>
-        // </ProjectCard>
-
 export default function Project(props) {
     const classes = useStyles();
     return (
@@ -52,18 +39,18 @@ export default function Project(props) {
                 </Typography>
             </CardContent>
         </CardActionArea>
-            <CardActions>
-                {props.project.video_url.length !== 0 ? <Button><a target="_blank" rel="noreferrer" alt="Hosted website" href={props.project.video_url}>YouTube</a></Button> : null}
-                {props.project.url.length !== 0 ? <Button><a target="_blank" rel="noreferrer" alt="Hosted website" href={props.project.url}>Demo</a></Button> : null }
-                {props.project.front.length !== 0 ? <Button><a target="_blank" rel="noreferrer" alt="Frontend Repo" href={props.project.front}>Frontend</a></Button> : null }
-                {props.project.back.length !== 0 ? <Button><a target="_blank" rel="noreferrer" alt="Backend Repo" href={props.project.back}>Backend</a></Button> : null }
-                {/* <Button size="small" color="primary">
+            <BottomCardActions>
+                {props.project.video_url.length !== 0 ? <ButtonStyled target="_blank" rel="noreferrer" alt="Hosted website" href={props.project.video_url}><YouTubeIcon/>&nbsp;YouTube</ButtonStyled> : null}
+                {props.project.url.length !== 0 ? <ButtonStyled target="_blank" rel="noreferrer" alt="Hosted website" href={props.project.url}><WebIcon/>&nbsp;Demo</ButtonStyled> : null }
+                {props.project.front.length !== 0 ? <ButtonStyled target="_blank" rel="noreferrer" alt="Frontend Repo" href={props.project.front}><GitHubIcon/>&nbsp;Front</ButtonStyled> : null }
+                {props.project.back.length !== 0 ? <ButtonStyled target="_blank" rel="noreferrer" alt="Backend Repo" href={props.project.back}><GitHubIcon/>&nbsp;Back</ButtonStyled> : null }
+                {/* <ButtonStyled size="small" color="primary">
                 Share
-                </Button>
-                <Button size="small" color="primary">
+                </ButtonStyled>
+                <ButtonStyled size="small" color="primary">
                 Learn More
-                </Button> */}
-            </CardActions>
+                </ButtonStyled> */}
+            </BottomCardActions>
     </Card>
     )
 }
--- src/components/Project/style.js
+++ src/components/Project/style.js
@@ -1,41 +1,11 @@
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
 
-export const ProjectCard = styled.div`
-    height: 40vh;
-    width: 40vw;
-    position: relative;
-    transition: 0.3s;
-    margin: auto;
-    justify-content: center;
-    background-color: rgba(255, 255, 255, 0.1);
-    margin-bottom: 1vh;
-    &:hover {
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    }
-    a {
-        cursor: pointer;
-    }
-    p {
-        margin: 8px;
-    }
-    @media (max-width: 800px) {
-        margin-bottom: 3vh;
-        height: 50vh;
-        font-size: 0.9rem;
-    }
-    overflow: hidden;
-
+export const ButtonStyled = styled(Button)`
+    text-align: inline-center;
 `
 
-export const ProjectImage = styled.div`
-img {
-    width: 30vw
-}
-`
-
-export const ProjectDetails = styled.div`
-    margin: auto;   
-    ${'' /* position: absolute; */}
-    text-align: center;
-    bottom: 0;
+export const BottomCardActions = styled(CardActions)`
+    justify-content: space-between;
 `
